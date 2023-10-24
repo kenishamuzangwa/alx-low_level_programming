53c3b5569942e28f326870b20fd637b0d59075cc
kenisha <kenishamuzangwa@gmail.com>
2023-10-24 00:52:03 -0700

    updated

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+
+	/**
+	 * sum_them_all - Returns the sum of all its para.
+	 * @n: The number of para passed to the function.
+	 * @...: A variable number of para to calculate the sum of.
+	 *
+	 * Return: If n == 0 - 0.
+	 *         Otherwise - the sum of all para.
+	 */
+	int sum_them_all.c(const unsigned int n, ...)
+	{
+		va_list ap;
+		unsigned int i, sum = 0;
+
+
+		va_start(ap, n);
+
+
+		for (i = 0; i < n; i++)
+			sum += va_arg(ap, int);
+
+
+		va_end(ap);
+
+
+		return (sum);
+	}
